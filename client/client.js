045f35f4197b8cb10d777ed619cde16110c645b3
smaharjan <smaharjan@icct.com>
2016-06-28 23:04:49 -0400

    second

--- client/client.js
+++ client/client.js
@@ -1,1 +1,15 @@
+import React from 'react'
+import { render } from 'react-dom'
+//import { render,method1 } from 'react-dom'
+// var render = require('react-dom').render;
+//var method1=require('react-dom').method1;
 
+import App from '../components/App'
+
+render(
+	//define the encompassing component,
+	// DOM element we want to mount it to
+	<App/>,
+	document.getElementById('app')
+
+)
--- components/App.js
+++ components/App.js
@@ -1,1 +1,15 @@
+import React,{ Component } from 'react'
 
+class App extends Component{
+	render(){
+		return <div>This is a hottestsssS module definitely a React app now</div>
+	}
+}
+
+/*var App = React.createClass({
+	render(){
+		return <div>This is a definitely module reloading React app now</div>
+	}
+})*/
+
+export default App
